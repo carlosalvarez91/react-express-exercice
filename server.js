ed3bdd7bfca06cda2cac5f05ef16e201fb5d0447
carlos <calvarez@seeingnewdata.com>
2018-12-07 15:30:21 +0000

    client/build -> app.use(express.static('frontend/build'));

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.get('/api/countries', (req, res) => {
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production'){
   //set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('frontend/build'));
 
   app.get('*', (req, res)=>{
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
